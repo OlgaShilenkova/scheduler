1962ffd58c6de6999cc717824619e6a22737d251
Olga Shilenkova <Olga1shilenkova@gmail.com>
2022-01-23 19:25:38 -0600

    change straight assign of mode value to be assigned using useMode function

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,28 +3,22 @@ import { useState } from "react";
 // if you use export default -> in test file import useVisualMode without {}
 
 export function useVisualMode(initial) {
-  // const [mode, setMode] = useState(initial);
+  const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
   function transition(newMode, replace = false) {
-    // DID NOT GET YET
-    // replace the current mode in the history with a new one
-    // if (replace) {
-    //   const newHistory = [...history, newMode];
-    //   console.log("newHistory", newHistory);
-
-    //   newHistory.pop();
-    //   console.log("newHistory after pop", newHistory);
-
-    //   const newHistory1 = [...newHistory, newMode];
-    //   console.log("newHistory1 after spread and add mewMode", newHistory1);
-
-    //   setHistory(newHistory1);
-    //   console.log("newHistory1", newHistory1);
-    // }
-    //add the new mode to our history array
-    const newHistory = [...history, newMode];
-    setHistory(newHistory);
+    setMode(newMode);
+    //replace the current mode in the history with a new one
+    if (replace) {
+      const newHistory = [...history];
+      newHistory.pop();
+      newHistory.push(newMode);
+      setHistory(newHistory);
+    } else {
+      //add the new mode to our history array
+      const newHistory = [...history, newMode];
+      setHistory(newHistory);
+    }
   }
 
   function back() {
@@ -33,10 +27,9 @@ export function useVisualMode(initial) {
     }
     const newHistory = [...history];
     newHistory.pop();
+    setMode(newHistory[newHistory.length - 1]);
     setHistory(newHistory);
   }
-
-  const mode = history[history.length - 1];
 
   return {
     mode,
